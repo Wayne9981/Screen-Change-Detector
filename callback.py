8fc3de86943897b66066906f20804881c4f79ee4
junwei <junwei@actionapp.io>
2023-06-13 01:13:20 +0800

    Add callback function

---
+++ callback.py
@@ -0,0 +1,47 @@
+import logging
+from collections import deque
+from math import sqrt
+from datetime import datetime
+from time import sleep
+import os
+
+from pyautogui import screenshot
+from PIL import ImageChops
+from PIL.Image import Image
+import numpy as np
+
+from utils import log_times
+
+IMAGE_FOLDER = "images"
+DIFF_THRESHOLD = 0.3
+TIME_INTERVAL_SEC = 5
+
+queue = deque(maxlen=2)
+
+
+def periodic_screen_analysis():
+    while True:
+        screen_analysis()
+        sleep(TIME_INTERVAL_SEC)
+
+
+@log_times
+def screen_analysis():
+    img = screenshot()
+    img = img.convert("RGB")
+    queue.append(img)
+    if len(queue) > 1:
+        diff = compare_images(queue[0], queue[1])
+        logging.info(f"{diff = :.2%}")
+        if diff > DIFF_THRESHOLD:
+            os.makedirs(IMAGE_FOLDER, exist_ok=True)
+            file_path = f"{datetime.now().isoformat()}.jpeg"
+            logging.info(f"{file_path = }")
+            img.save(os.path.join(IMAGE_FOLDER, file_path), quality=70)
+
+
+@log_times
+def compare_images(img1: Image, img2: Image) -> float:
+    diff = np.asarray(ImageChops.difference(img1, img2).getdata()) / 255
+    differ = sqrt(np.mean(np.square(diff)))
+    return differ
