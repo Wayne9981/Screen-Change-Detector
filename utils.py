94fd24a30d9ff33d0b637e8ca6accede7c662b80
junwei <junwei@actionapp.io>
2023-06-13 01:10:11 +0800

    Add log_times decorators

---
+++ utils.py
@@ -0,0 +1,28 @@
+import logging
+from functools import wraps
+from time import time
+
+
+def log_times(func):
+    """decorator to log everything upon entry and exit of a function/method"""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        mod_name = func.__module__
+
+        logging.debug(
+            f"Start {mod_name}.{func.__name__}: args={str(args)}, kwargs={str(kwargs)}"
+        )
+        now = time()
+        try:
+            res = func(*args, **kwargs)
+        except Exception as e:
+            logging.exception(f"On exit {mod_name}.{func.__name__}: exception={str(e)}")
+            raise
+        else:
+            logging.debug(
+                f"Done {mod_name}.{func.__name__}: res={str(res)}, spend {time() - now} sec"
+            )
+            return res
+
+    return wrapper
