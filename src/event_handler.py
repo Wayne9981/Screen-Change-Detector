0d0e68fe4fd3ef80a9f0467fc5f71dbc789fe8d3
junwei <junwei@actionapp.io>
2023-06-18 18:47:36 +0800

    Add MouseEventHandler

--- src/event_handler.py
+++ src/event_handler.py
@@ -5,6 +5,8 @@ import termios
 
 from pynput.keyboard import KeyCode
 
+import utils
+
 logger = logging.getLogger(__name__)
 
 
@@ -22,3 +24,24 @@ class KeyboardEventHandler:
             print(f"{type(handler).__name__} started")
             handler()
             print(f"{type(handler).__name__} finished")
+
+
+class MouseEventHandler:
+    def __init__(self):
+        self.start = None
+        self.end = None
+
+    @utils.log_times
+    def handle(self, point: tuple[float, float], pressed: bool):
+        point = list(map(int, point))
+        if pressed:
+            self.start = point
+        else:
+            self.end = point
+
+    @utils.log_times
+    def get_box(self):
+        if self.start and self.end:
+            left, right = sorted((self.start[0], self.end[0]))
+            up, down = sorted((self.start[1], self.end[1]))
+            return [left, up, right, down]
