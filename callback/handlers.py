6918ee211e6681600f1f55828f0a9802caef017c
junwei <junwei@actionapp.io>
2023-06-18 15:56:43 +0800

    Add analysis_minutes

--- callback/handlers.py
+++ callback/handlers.py
@@ -1,7 +1,11 @@
+import logging
 from time import sleep
+from datetime import datetime, timedelta
 
 from src.config import ConfigAccessor
 from .screen_analysis import _screen_analysis
+
+logger = logging.getLogger(__name__)
 
 
 class ScreenAnalysis:
@@ -19,7 +23,10 @@ class PeriodicScreenAnalysis:
 
     def __call__(self):
         cfg = self.cfg_proxy.cfg
-        while True:
+        end_time = datetime.now() + timedelta(minutes=cfg.analysis_minutes)
+        logger.info(f"{end_time = }")
+        print(f"Job will end at {end_time}")
+        while datetime.now() < end_time:
             _screen_analysis(cfg.diff_threshold_percentage, cfg.image_folder)
             sleep(cfg.time_interval_sec)
 
@@ -36,8 +43,20 @@ class SettingConfig:
                 " or press Enter to use the previous setting: "
             )
         )
+        logger.info(f"{time_interval_sec = }")
         try:
             cfg.time_interval_sec = float(time_interval_sec)
+        except ValueError:
+            pass
+        analysis_minutes = input(
+            (
+                "\rPlease enter the analysis time in minutes,"
+                " or press Enter to use the previous setting: "
+            )
+        )
+        logger.info(f"{analysis_minutes = }")
+        try:
+            cfg.analysis_minutes = int(analysis_minutes)
         except ValueError:
             pass
 
